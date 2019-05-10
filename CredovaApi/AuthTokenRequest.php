96262b272c012388808b7ceca4ce072a31e5838a
Sean Templeton <sean@templeton.io>
2019-05-10 13:22:08 -0500

    CRD-634_9 - Add token caching to the Authentication request

--- CredovaApi/AuthTokenRequest.php
+++ CredovaApi/AuthTokenRequest.php
@@ -6,11 +6,22 @@ class AuthTokenRequest extends \ClassyLlama\Credova\CredovaApi\RequestAbstract
 {
     const PATH = 'v2/token';
     const TOKEN_KEY = 'jwt';
+    const CACHE_KEY = 'classyllama.credova.token';
 
     /**
      * Override content type from abstract
      */
     const CONTENT_TYPE = 'application/x-www-form-urlencoded';
+    /**
+     * @var Magento\Framework\App\CacheInterface
+     */
+    private $cache;
+
+    public function __construct(\Zend\Http\ClientFactory $clientFactory, \ClassyLlama\Credova\Helper\Config $configHelper, \Psr\Log\LoggerInterface $logger, \Magento\Framework\App\CacheInterface $cache)
+    {
+        parent::__construct($clientFactory, $configHelper, $logger);
+        $this->cache = $cache;
+    }
 
     /**
      * {@inheritdoc}
@@ -49,13 +60,24 @@ class AuthTokenRequest extends \ClassyLlama\Credova\CredovaApi\RequestAbstract
      * @return string
      * @throws \ClassyLlama\Credova\Exception\CredovaApiException
      */
-    public function getToken() : string
+    public function getToken(): string
     {
+        $token = $this->cache->load(self::CACHE_KEY);
+
+        if($token !== false) {
+            return $token;
+        }
+
         $data = $this->getResponseData();
 
         if (!isset($data[self::TOKEN_KEY])) {
             throw new \ClassyLlama\Credova\Exception\CredovaApiException(__('Access token not found.'));
         }
+
+        // Set the cache expiration to the exp timestamp in the token, minus 5 minutes to ensure we don't have rejected token errors
+        $expirationInSeconds = json_decode(base64_decode($data[self::TOKEN_KEY]), true)['exp'] - time() - (5 * 60);
+
+        $this->cache->save($data[self::TOKEN_KEY], self::CACHE_KEY, null, $expirationInSeconds);
 
         return $data[self::TOKEN_KEY];
     }
--- registration.php
+++ registration.php
@@ -3,5 +3,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'ClassyLlama_Credova',
-    __DIR__
+    isset($file) ? dirname($file) : __DIR__
 );
